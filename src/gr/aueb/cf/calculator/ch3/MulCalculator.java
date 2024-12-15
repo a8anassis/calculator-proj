ec9fa5e5378f6dc70f6a683cbe5b33c6f75517de
Th. Androutsos <a8anassis@gmail.com>
2024-12-15 13:05:50 +0200

    MulCalculator created

---
+++ src/gr/aueb/cf/calculator/ch3/MulCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.calculator.ch3;
+
+public class MulCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int result = 0;
+
+        result = mul(num1, num2);
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+}
