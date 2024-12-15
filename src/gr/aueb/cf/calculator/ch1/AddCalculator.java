b9b5af0a6825d9b97da7feb7a79bab04564ada92
Th. Androutsos <a8anassis@gmail.com>
2024-12-15 12:18:03 +0200

    Project created

---
+++ src/gr/aueb/cf/calculator/ch1/AddCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.calculator.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int result = 0;
+
+        result = add(num1, num2);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/gr/aueb/cf/calculator/ch2/SubCalculator.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.calculator.ch2;
+
+public class SubCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int result = 0;
+
+        result = sub(num1, num2);
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
